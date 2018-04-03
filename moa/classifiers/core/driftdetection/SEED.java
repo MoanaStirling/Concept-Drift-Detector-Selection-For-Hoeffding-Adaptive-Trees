5f07fa18f8a62348b7f5de732611929ff407e899
Moana Stirling <moana.stirling@gmail.com>
2018-04-03 20:55:10 +1200

    Update SEED.java

--- moa/classifiers/core/driftdetection/SEED.java
+++ moa/classifiers/core/driftdetection/SEED.java
@@ -1,41 +1,9 @@
-/*
- * SEED.java
- * author: David T.J. Huang - The University of Auckland
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *             http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
- * either express or implied. See the License for the specific
- * language governing permissions and limitations under the
- * License.
- *
- */
-
 package moa.classifiers.core.driftdetection;
 
 import com.github.javacliparser.FloatOption;
 import com.github.javacliparser.IntOption;
 import moa.core.ObjectRepository;
 import moa.tasks.TaskMonitor;
-
-/**
- *
- * Drift detection method as published in:
- * </p>
- * David Tse Jung Huang, Yun Sing Koh, Gillian Dobbie, and Russel Pears: Detecting Volatility Shift in Data Streams. ICDM 2014: 863-868
- * </p>
- * Usage: {@link #setInput(double)}
- * </p>
- *
- * @author David T.J. Huang - The University of Auckland
- * @version 1.0
- */
 
 public class SEED {
     public SEEDWindow window;
